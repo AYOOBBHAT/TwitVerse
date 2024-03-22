5790bed6ddfc7827f67ad403d402978f4983fd79
Ayoob Bhat <ayoob324005@gmail.com>
2024-03-22 16:16:22 +0530

    added button & Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-export default function Home(){
-  return (<div className="text-3xl text-sky-500">Hello world </div>)
+import Header from "@/components/Header";
+export default function Home() {
+  return (
+    <>
+
+      <Header label="Home" />
+    </>
+
+  );
 }
